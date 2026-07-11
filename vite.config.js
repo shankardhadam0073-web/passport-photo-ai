faef2d7105344f1d22e11f22bef91b5a1425319c
Shankar Dhadam <shankardhadam0073@gmail.com>
2026-07-11 11:00:18 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['**/*.{png,svg,jpg,jpeg,ico,txt,woff2}'],
